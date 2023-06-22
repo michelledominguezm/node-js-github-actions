8add2b2bf38b4b415c97cef3dafe3aa57d407d24
michelledominguezm <67466494+michelledominguezm@users.noreply.github.com>
2023-06-22 15:18:21 +0200

    Create actions-runner.sh

---
+++ actions-runner.sh
@@ -0,0 +1,5 @@
+# Create a folder
+$ mkdir actions-runner && cd actions-runner# Download the latest runner package
+$ curl -o actions-runner-linux-x64-2.305.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.305.0/actions-runner-linux-x64-2.305.0.tar.gz# Optional: Validate the hash
+$ echo "737bdcef6287a11672d6a5a752d70a7c96b4934de512b7eb283be6f51a563f2f  actions-runner-linux-x64-2.305.0.tar.gz" | shasum -a 256 -c# Extract the installer
+$ tar xzf ./actions-runner-linux-x64-2.305.0.tar.gz
