fb4324b9b41c4b2b1d2b91b2665f599726d4e76e
michelledominguezm <67466494+michelledominguezm@users.noreply.github.com>
2023-06-22 15:22:34 +0200

    Delete actions-runner-2.sh

--- actions-runner-2.sh
+++
@@ -1,3 +0,0 @@
-# Create the runner and start the configuration experience
-$ ./config.sh --url https://github.com/michelledominguezm/node-js-github-actions --token AQCXJ7SPJFP2D7NDYYN42P3ESRL2E# Last step, run it!
-$ ./run.sh
